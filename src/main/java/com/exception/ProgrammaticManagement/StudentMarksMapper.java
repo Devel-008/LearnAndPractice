df1c0a827dd2416ca5a095c53fa32c09606457eb
ISHA SETHIA <isha.sethia@confluxsys.com>
2022-10-07 11:51:40 +0530

    Initial commit

---
+++ src/main/java/com/exception/ProgrammaticManagement/StudentMarksMapper.java
@@ -0,0 +1,21 @@
+package com.exception.ProgrammaticManagement;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.exception.ProgrammaticManagement.StudentMarks;
+import org.springframework.jdbc.core.RowMapper;
+
+public class StudentMarksMapper implements RowMapper<StudentMarks> {
+    public StudentMarks mapRow(ResultSet rs, int rowNum) throws SQLException {
+        StudentMarks studentMarks = new StudentMarks();
+        studentMarks.setId(rs.getInt("id"));
+        studentMarks.setName(rs.getString("name"));
+        studentMarks.setAge(rs.getInt("age"));
+        studentMarks.setSid(rs.getInt("sid"));
+        studentMarks.setMarks(rs.getInt("marks"));
+        studentMarks.setYear(rs.getInt("years"));
+
+        return studentMarks;
+    }
+}
